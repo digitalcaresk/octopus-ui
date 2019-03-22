6029f0f7a75e40ab5d1356bbc027d61f976e3ea2
Peter Zádori <peter.zadori@gmail.com>
2019-03-22 20:45:39 +0100

    Updated at sorting

--- addon/components/ui-data-sorter.js
+++ addon/components/ui-data-sorter.js
@@ -7,7 +7,7 @@ export default Component.extend({
 
     tagName: '',
 
-    sortBy: 'id',
+    sortBy: 'updatedAt',
     sortDirection: 'desc',
 
     sortingDefinition: computed('sortBy', 'sortDirection', function() {
