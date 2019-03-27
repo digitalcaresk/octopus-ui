9fc46a9a9f9314e6a9fc7075f42563f260a50e8a
Peter Zádori <peter.zadori@gmail.com>
2019-03-27 07:06:14 +0100

    Changes

--- addon/components/ui-data-table/column/sortable-header-cell.js
+++ addon/components/ui-data-table/column/sortable-header-cell.js
@@ -1,10 +1,16 @@
 import HeaderCell from './header-cell';
 import layout from 'octopus-ui/templates/components/ui-data-table/column/sortable-header-cell';
+import {computed} from '@ember/object';
 
 export default HeaderCell.extend({
     layout,
 
     classNames: ['table-sortable'],
+    classNameBindings: ['isSortedBy:active'],
+
+    isSortedBy: computed('property', 'sortedBy', function () {
+        return this.property === this.sortedBy;
+    }),
 
     onSort() {
 
