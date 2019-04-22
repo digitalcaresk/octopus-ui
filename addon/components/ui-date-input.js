e6ef1a2a45725b1a3196210ba5b41830755ab304
Peter Zádori <peter.zadori@gmail.com>
2019-04-22 02:16:24 +0200

    Date input

--- addon/components/ui-date-input.js
+++ addon/components/ui-date-input.js
@@ -7,23 +7,48 @@ export default Component.extend({
     tagName: '',
 
     minDate: null,
+    maxDate: null,
     disabled: false,
+    autoOpen: false,
+    flatpickr: null,
+    icon: true,
     mode: 'single',
     enable: [],
 
-    dateFormat: computed('enableTime', function() {
+    onSelect() {
+
+    },
+
+    onDayCreate() {
+
+    },
+
+    setFlatpickr(flatpickr) {
+        this.set('flatpickr', flatpickr);
+
+        if (this.autoOpen) {
+            this.flatpickr.open();
+        }
+    },
+
+    dateFormat: computed('enableTime', function () {
         return this.enableTime ? 'H:i d.m.Y' : 'd.m.Y';
     }),
 
     updateValue(selectedDates) {
-        if (this.isInRangeMode) {
+        if (this.isInRangeMode || this.isInMultipleMode) {
             this.set('value', selectedDates);
+
+            this.onSelect(selectedDates);
         } else {
             let [date] = selectedDates;
 
             this.set('value', date);
+
+            this.onSelect(date);
         }
     },
 
-    isInRangeMode: computed.equal('mode', 'range')
+    isInRangeMode: computed.equal('mode', 'range'),
+    isInMultipleMode: computed.equal('mode', 'multiple'),
 });
