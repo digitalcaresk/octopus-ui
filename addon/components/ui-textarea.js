70f25c7fae07563994d125c91f008892a13d91c8
Peter Zádori <peter.zadori@gmail.com>
2019-04-23 15:29:33 +0200

    Textarea errors

--- addon/components/ui-textarea.js
+++ addon/components/ui-textarea.js
@@ -1,5 +1,11 @@
 import TextArea from '@ember/component/text-area';
+import {computed} from '@ember/object';
 
 export default TextArea.extend({
-    classNames: 'form-control'
+    classNames: 'form-control',
+    classNameBindings: ['isInvalid'],
+
+    errors: null,
+
+    isInvalid: computed.notEmpty('errors')
 });
