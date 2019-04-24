5b449a8fa0686ca1ab139bb276752ff824899e83
Peter Zádori <peter.zadori@gmail.com>
2019-04-24 17:18:22 +0200

    Select / deselect all

--- addon/components/ui-data-table/column/selectable-header-cell.js
+++ addon/components/ui-data-table/column/selectable-header-cell.js
@@ -1,8 +1,20 @@
 import HeaderCell from './header-cell';
 import layout from 'octopus-ui/templates/components/ui-data-table/column/selectable-header-cell';
+import {computed} from '@ember/object';
 
 export default HeaderCell.extend({
     layout,
 
     width: 30,
+    areAllSelected: computed('rows.@each.selected', function () {
+        return this.rows.filterBy('selected', true).length === this.rows.length;
+    }),
+
+    toggleSelected() {
+        let areAllSelected = this.areAllSelected;
+
+        this.rows.forEach(row => {
+            row.set('selected', !areAllSelected);
+        })
+    }
 });
