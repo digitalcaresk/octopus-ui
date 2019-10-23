1b78a663851abbae643934de999ad34e43d344b2
Peter Zádori <peter.zadori@metasoft.sk>
2019-10-23 14:55:58 +0200

    Checkbox errors

--- addon/components/ui-checkbox/input.js
+++ addon/components/ui-checkbox/input.js
@@ -1,8 +1,12 @@
 import Checkbox from '@ember/component/checkbox';
+import {computed} from '@ember/object';
 import {run} from '@ember/runloop';
 
 export default Checkbox.extend({
     classNames: ['custom-control-input'],
+    classNameBindings: ['isInvalid'],
+
+    isInvalid: computed.notEmpty('errors'),
 
     onChange() {
 
